b57a5e39f75c31a41e23289ad667636733910ecf
SPETS1337 <gamingpackage@gmail.com>
2017-01-01 13:34:43 +0100

    Initial commit
    committed all files

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      
+        <title>CarParkSystem</title>
+
+        <meta charset="UTF-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="SPETS1337">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="css/style.css"rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+
+      
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-default" role="navigation">
+              <div class="container">
+                <div class="navbar-header">
+
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+
+                  <div class="navbar-brand navbar-brand-centered"><a class="brand" href="<?php echo $_SERVER["PHP_SELF"]; ?>"><i id="brandicon" class="fa fa-car" aria-hidden="true"></i> CPS</a></div>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbar-brand-centered">
+
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">menu</a></li>
+                        <li><a href="#">menu</a></li>
+                        <li><a href="#">menu</a></li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">menu</a></li> <!-- Sign in button will probably be here -->
+                    </ul>
+
+                </div>
+              </div>
+            </nav>
+            <div class="container">
+
+                <div class="jumbotron">
+                    <h1>CarParkSystem</h1>
+                    <p id="slogan">Car park administration made easy, and good looking.</p>
+                </div>
+
+                <div class="row">
+
+                    <div class="box col-md-6">
+                        <div class="jumbotron">
+                            <h3>About CPS</h3>
+                            <div class="seperator">&nbsp;</div>
+
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <br><br>
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                        </div>
+                    </div>
+
+                    <div class="box col-md-6">
+                        <div class="jumbotron">
+                            <h3>MIT License</h3>
+                            <div class="seperator">&nbsp;</div>
+
+                            <p>Copyright (c) 2017 <a href="https://github.com/SPETS1337/" target="_blank">SPETS1337</a>
+                            <br><br>
+                            Permission is hereby granted, free of charge, to any person obtaining a copy
+                            of this software and associated documentation files (the "Software"), to deal
+                            in the Software without restriction, including without limitation the rights
+                            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                            copies of the Software, and to permit persons to whom the Software is
+                            furnished to do so, subject to the following conditions:
+                            <br><br>
+                            The above copyright notice and this permission notice shall be included in all
+                            copies or substantial portions of the Software.
+                            <br><br>
+                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                            SOFTWARE.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <footer>&copy; CarParkSystem&trade; &mdash; A project by <a href="https://github.com/SPETS1337/" target="_blank">SPETS1337</a></footer>
+
+            <script src="js/jquery.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+
+        </body>
+
+</html>

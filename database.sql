9bbdf95f2c293f44e4e666c2f4ace153bf17ab93
SPETS1337 <gamingpackage@gmail.com>
2017-01-04 01:10:42 +0100

    Added table

--- database.sql
+++ database.sql
@@ -1,36 +1,70 @@
-/*
-SQLyog Community v12.3.3 (64 bit)
-MySQL - 10.1.13-MariaDB : Database - carparksystem
-*********************************************************************
-*/
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`carparksystem` /*!40100 DEFAULT CHARACTER SET latin1 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `carparksystem`;
+--
+-- Database: `carparksystem`
+--
 
-/*Table structure for table `cps_users` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `cps_users`;
+--
+-- Table structure for table `cps_logs`
+--
 
-CREATE TABLE `cps_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` text NOT NULL,
-  `passwordHash` text NOT NULL,
-  `isAdmin` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `cps_logs` (
+  `id` int(11) NOT NULL,
+  `datetime` text NOT NULL,
+  `forUser` text NOT NULL,
+  `fromIp` text NOT NULL,
+  `success` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `cps_users` */
+-- --------------------------------------------------------
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Table structure for table `cps_users`
+--
+
+CREATE TABLE `cps_users` (
+  `id` int(11) NOT NULL,
+  `user` text NOT NULL,
+  `passwordHash` text NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cps_users`
+--
+
+INSERT INTO `cps_users` (`id`, `user`, `passwordHash`, `isAdmin`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cps_users`
+--
+ALTER TABLE `cps_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cps_users`
+--
+ALTER TABLE `cps_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
